6fafd603ecbdf249b1dee6e9e042f6d8c9cb0c50
sakthipradeep <105548072+sakthipradeep@users.noreply.github.com>
2022-06-11 19:57:05 +0530

    Add files via upload

---
+++ FactorialNumber.java
@@ -0,0 +1,17 @@
+package week1.day1.assignment;
+
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+int i;
+int fact=1;
+int number=5;
+	for(i=1;i<=number;i++) {
+	fact=fact*i;
+	System.out.println( fact);
+	}
+	}
+
+}
+
